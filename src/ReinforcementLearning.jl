9a5fe13e291214031493cf5636b280185a64f211
Christof Stocker <stocker.christof@gmail.com>
2017-11-09 15:59:02 +0100

    add discretizing environment

--- src/ReinforcementLearning.jl
+++ src/ReinforcementLearning.jl
@@ -3,95 +3,22 @@ module ReinforcementLearning
 
 using POMDPs
 using POMDPToolbox
-import POMDPs: isterminal
-import POMDPs: action, actions, n_actions
-import POMDPs: states, n_states
+using Discretizers
+
+import POMDPs: isterminal, discount
+import POMDPs: action, actions, action_index, n_actions
+import POMDPs: states, state_index, n_states
 
 export
 
     isterminal,
-    action,
+    actions,
     step!,
-    reset!
+    reset!,
 
-"""
-    abstract type Environment end
+    DiscretizedEnvironment
 
-Supertype for all reinforcement learning (RL) environments. In
-essence a subtype of an `Environment` is expected to behave
-according to some underlying Markov Decision Process (MDP) that
-only can only be interacted with in a stepwise fashion.
-
-There are some notable conceptual differences between a subtype
-of `Environment` and a subtype of `MDP`.
-
-- While an environment is assumed to be governed by an MDP,
-  this MDP is generally only partially known (emphasis on
-  "known"; this doesn't imply it is only partially observable).
-  The typical unknowns are the dynamics (transitions) and/or the
-  reward function.
-
-- Consequently an environment does not need to be able expose
-  arbitrary state transitions. Instead, it can be stateful and
-  only ever expose a sampled trajectory starting from initial
-  states of its own choosing.
-
-- Any subtype of an `MDP` can easily be converted to an
-  `Environment` by keeping track of its own state. The other
-  direction is not as simple.
-
-Any subtype of `Environment` has to implement the following
-functions: [`actions`](@ref), [`step!`](@ref), [`reset!`](@ref),
-[`isterminal`](@ref).
-"""
-abstract type Environment end
-
-"""
-    step!([rng], env::Environment, a) -> (s´, r)
-
-Apply the given action `a` to the current state of the
-environment `env` and return the next state `s´` as well as the
-received award `r` caused by this transition.
-
-Optionally a user can provide a custom random number generator
-`rng`. If for example the given `env` is a simulator that needs
-to generate random numbers for some reason, it will do so using
-the given `rng`.
-
-Note that this function does not expect the user to provide a
-state. This is because the environment is in general expected to
-be stateful and thus knows its actual state. The state `s´`
-observed from the environment is not required to have a causal
-relationship to the next state. This does not imply that the
-underlying MDP is therefor partially observed. For example the
-next screen in an Atari game is not actually determined by
-looking at the last 5 screens. Yet for Atari games it is still a
-fair assumption that the last 5 screens correspond to a unique
-game state.
-"""
-step!(env::Environment, args...) =
-    step!(Base.GLOBAL_RNG, env, args...)
-
-"""
-    reset!([rng], env::Environment, [...]) -> s₀
-
-Reset the given environment to an initial state and return it.
-Note that individual environment may support additional
-parameters.
-
-Optionally a user can provide a custom random number generator
-`rng`. If for example the given `env` is a simulator that needs
-to generate random numbers for some reason, it will do so using
-the given `rng`.
-"""
-reset!(env::Environment, args...) =
-    reset!(Base.GLOBAL_RNG, env, args...)
-
-"""
-    isterminal(env::Environment) -> Bool
-
-Check is `env` is in a terminal state.
-"""
-isterminal(env::Environment) = false
+include("abstract.jl")
+include("environments.jl")
 
 end # module
---
+++ src/environments.jl
@@ -0,0 +1,44 @@
+struct DiscretizedEnvironment{N,E<:Environment,D,S} <: Environment
+    env::E
+    discretizers::D
+    dims::S
+end
+
+function DiscretizedEnvironment(
+        env::Environment,
+        discretizers::NTuple{N,Discretizers.AbstractDiscretizer}) where N
+    dims = map(nlabels, discretizers)
+    DiscretizedEnvironment{N,typeof(env),typeof(discretizers),typeof(dims)}(env, discretizers, dims)
+end
+
+function DiscretizedEnvironment(
+        env::Environment,
+        bins::NTuple{N,AbstractVector}) where N
+    fbins = map(A->convert(Vector{Float64},A), bins)
+    discretizers = map(LinearDiscretizer, fbins)
+    DiscretizedEnvironment(env, discretizers)
+end
+
+discount(env::DiscretizedEnvironment) = discount(env.env)
+
+state_index(env::DiscretizedEnvironment, s) = sub2ind(env.dims, s...)
+n_states(env::DiscretizedEnvironment) = prod(env.dims)
+
+actions(env::DiscretizedEnvironment) = actions(env.env)
+action_index(env::DiscretizedEnvironment, a) = action_index(env.env, a)
+n_actions(env::DiscretizedEnvironment) = n_actions(env.env)
+
+function Base.show(io::IO, env::DiscretizedEnvironment)
+    # TODO: show something sensible
+    print(io, typeof(env).name)
+end
+
+function step!(rng, env::DiscretizedEnvironment{N}, a) where N
+    s, r = step!(rng, env.env, a)
+    ntuple(i->encode(env.discretizers[i], s[i]), Val{N}), r
+end
+
+function reset!(rng, env::DiscretizedEnvironment{N}, args...) where N
+    s = reset!(rng, env.env, args...)
+    ntuple(i->encode(env.discretizers[i], s[i]), Val{N})
+end
